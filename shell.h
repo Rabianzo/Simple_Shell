2be3b93d4d1ab017897eb2c4a7be1fd266dcc152
gracie-123 <gracietumbo@gmail.com>
2023-10-19 14:50:38 -0700

    simple shell project

--- shell.h
+++ shell.h
@@ -11,24 +11,15 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* for command chaining */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
 #define CMD_CHAIN	3
-<<<<<<< HEAD
 /* for convert_number() */
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
@@ -38,23 +29,6 @@
 #define HIST_FILE	".simple_shell_history"
 #define HIST_MAX	4096
 extern char **environ;
-=======
-
-/* for convert_number() */
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
-
-/* 1 if using system getline() */
-#define USE_GETLINE 0
-#define USE_STRTOK 0
-
-#define HIST_FILE	".simple_shell_history"
-#define HIST_MAX	4096
-
-extern char **environ;
-
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /**
  * struct liststr - singly linked list
  * @num: the number field
@@ -67,7 +41,6 @@ typedef struct liststr
 	char *str;
 	struct liststr *next;
 } list_t;
-<<<<<<< HEAD
 /**
  *struct passinfo - contains pseudo-arguements to pass into a function,
  *		allowing uniform prototype for function pointer struct
@@ -89,30 +62,6 @@ typedef struct liststr
  *@cmd_buf_type: CMD_type ||, &&, ;
  *@readfd: the fd from which to read line input
  *@histcount: the history line number count
-=======
-
-/**
- * struct passinfo - contains pseudo-arguements to pass into a function,
- * allowing uniform prototype for function pointer struct
- * @arg: a string generated from getline containing arguements
- * @argv:an array of strings generated from arg
- * @path: a string path for the current command
- * @argc: the argument count
- * @line_count: the error count
- * @err_num: the error code for exit()s
- * @linecount_flag: if on count this line of input
- * @fname: the program filename
- * @env: linked list local copy of environ
- * @environ: custom modified copy of environ from LL env
- * @history: the history node
- * @alias: the alias node
- * @env_changed: on if environ was changed
- * @status: the return status of the last exec'd command
- * @cmd_buf: address of pointer to cmd_buf, on if chaining
- * @cmd_buf_type: CMD_type ||, &&, ;
- * @readfd: the fd from which to read line input
- * @histcount: the history line number count
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
  */
 typedef struct passinfo
 {
@@ -130,16 +79,11 @@ typedef struct passinfo
 	char **environ;
 	int env_changed;
 	int status;
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
 	int cmd_buf_type; /* CMD_type ||, &&, ; */
 	int readfd;
 	int histcount;
 } info_t;
-<<<<<<< HEAD
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
@@ -147,211 +91,109 @@ typedef struct passinfo
  *struct builtin - contains a builtin string and related function
  *@type: the builtin command flag
  *@func: the function
-=======
-
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-		0, 0, 0}
-
-/**
- * struct builtin - contains a builtin string and related function
- * @type: the builtin command flag
- * @func: the function
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
  */
 typedef struct builtin
 {
 	char *type;
 	int (*func)(info_t *);
 } builtin_table;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_shloop.c */
 int hsh(info_t *, char **);
 int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_parser.c */
 int is_cmd(info_t *, char *);
 char *dup_chars(char *, int, int);
 char *find_path(info_t *, char *, char *);
-<<<<<<< HEAD
 /* loophsh.c */
 int loophsh(char **);
-=======
-
-/* loophsh.c */
-int loophsh(char **);
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_errors.c */
 void _eputs(char *);
 int _eputchar(char);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_string.c */
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_string1.c */
 char *_strcpy(char *, char *);
 char *_strdup(const char *);
 void _puts(char *);
 int _putchar(char);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_exits.c */
 char *_strncpy(char *, char *, int);
 char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
-<<<<<<< HEAD
 /* toem_tokenizer.c */
 char **strtow(char *, char *);
 char **strtow2(char *, char);
-=======
-
-/* toem_tokenizer.c */
-char **strtow(char *, char *);
-char **strtow2(char *, char);
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_realloc.c */
 char *_memset(char *, char, unsigned int);
 void ffree(char **);
 void *_realloc(void *, unsigned int, unsigned int);
-<<<<<<< HEAD
 /* toem_memory.c */
 int bfree(void **);
-=======
-
-/* toem_memory.c */
-int bfree(void **);
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_atoi.c */
 int interactive(info_t *);
 int is_delim(char, char *);
 int _isalpha(int);
 int _atoi(char *);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_errors1.c */
 int _erratoi(char *);
 void print_error(info_t *, char *);
 int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_builtin.c */
 int _myexit(info_t *);
 int _mycd(info_t *);
 int _myhelp(info_t *);
-<<<<<<< HEAD
 /* toem_builtin1.c */
 int _myhistory(info_t *);
 int _myalias(info_t *);
-=======
-
-/* toem_builtin1.c */
-int _myhistory(info_t *);
-int _myalias(info_t *);
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /*toem_getline.c */
 ssize_t get_input(info_t *);
 int _getline(info_t *, char **, size_t *);
 void sigintHandler(int);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_getinfo.c */
 void clear_info(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_environ.c */
 char *_getenv(info_t *, const char *);
 int _myenv(info_t *);
 int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_getenv.c */
 char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_history.c */
 char *get_history_file(info_t *info);
 int write_history(info_t *info);
 int read_history(info_t *info);
 int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_lists.c */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_lists1.c */
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 /* toem_vars.c */
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
 int replace_vars(info_t *);
 int replace_string(char **, char *);
-<<<<<<< HEAD
-=======
-
->>>>>>> 65b8d29497abe88e98705139350973d4829a6d79
 #endif
