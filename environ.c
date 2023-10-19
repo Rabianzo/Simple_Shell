ca6775a51af08592fdad05c2a7e237cafa8c63eb
gracie-123 <gracietumbo@gmail.com>
2023-10-19 15:02:50 -0700

    simple shell project

--- environ.c
+++ environ.c
@@ -4,13 +4,13 @@
  * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
+ * Return: Always 0
  */
 int _myenv(info_t *info)
 {
 	print_list_str(info->env);
 	return (0);
 }
-
 /**
  * _getenv - gets the value of an environ variable
  * @info: Structure containing potential arguments. Used to maintain
@@ -21,7 +21,6 @@ int _myenv(info_t *info)
 char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
-
 	char *p;
 
 	while (node)
@@ -29,20 +28,10 @@ char *_getenv(info_t *info, const char *name)
 		p = starts_with(node->str, name);
 		if (p && *p)
 			return (p);
-
-	char *q;
-
-	while (node)
-	{
-		q = starts_with(node->str, name);
-		if (q && *q)
-			return (q);
-
 		node = node->next;
 	}
 	return (NULL);
 }
-
 /**
  * _mysetenv - Initialize a new environment variable,
  *             or modify an existing one
@@ -57,11 +46,10 @@ int _mysetenv(info_t *info)
 		_eputs("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (_mysetenv(info, info->argv[1], info->argv[2]))
+	if (_setenv(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
 }
-
 /**
  * _myunsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
@@ -78,8 +66,7 @@ int _myunsetenv(info_t *info)
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
-		_myunsetenv(info, info->argv[i]);
-
+		_unsetenv(info, info->argv[i]);
 	return (0);
 }
 /**
@@ -88,7 +75,6 @@ int _myunsetenv(info_t *info)
  *          constant function prototype.
  * Return: Always 0
  */
-
 int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
--- vars.c
+++ vars.c
@@ -34,7 +34,6 @@ int is_chain(info_t *info, char *buf, size_t *p)
 	*p = j;
 	return (1);
 }
-
 /**
  * check_chain - checks we should continue chaining based on last status
  * @info: the parameter struct
@@ -65,10 +64,8 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 			j = len;
 		}
 	}
-
 	*p = j;
 }
-
 /**
  * replace_alias - replaces an aliases in the tokenized string
  * @info: the parameter struct
@@ -97,7 +94,6 @@ int replace_alias(info_t *info)
 	}
 	return (1);
 }
-
 /**
  * replace_vars - replaces vars in the tokenized string
  * @info: the parameter struct
@@ -107,38 +103,36 @@ int replace_alias(info_t *info)
 int replace_vars(info_t *info)
 {
 	int i = 0;
+
 	list_t *node;
 
 	for (i = 0; info->argv[i]; i++)
 	{
 		if (info->argv[i][0] != '$' || !info->argv[i][1])
 			continue;
-
 		if (!_strcmp(info->argv[i], "$?"))
 		{
 			replace_string(&(info->argv[i]),
-					_strdup(convert_number(info->status, 10, 0)));
+				_strdup(convert_number(info->status, 10, 0)));
 			continue;
 		}
 		if (!_strcmp(info->argv[i], "$$"))
 		{
 			replace_string(&(info->argv[i]),
-					_strdup(convert_number(getpid(), 10, 0)));
+				_strdup(convert_number(getpid(), 10, 0)));
 			continue;
 		}
 		node = node_starts_with(info->env, &info->argv[i][1], '=');
 		if (node)
 		{
 			replace_string(&(info->argv[i]),
-					_strdup(_strchr(node->str, '=') + 1));
+				_strdup(_strchr(node->str, '=') + 1));
 			continue;
 		}
 		replace_string(&info->argv[i], _strdup(""));
-
 	}
 	return (0);
 }
-
 /**
  * replace_string - replaces string
  * @old: address of old string
@@ -149,82 +143,8 @@ int replace_vars(info_t *info)
 int replace_string(char **old, char *new)
 {
 	free(*old);
+
 	*old = new;
+
 	return (1);
 }
-
-#include "shell.h"
-
-/**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
- */
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
-
-/**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
- */
-int is_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
-
-/**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- */
-
-int _atoi(char *s)
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
-}
-
